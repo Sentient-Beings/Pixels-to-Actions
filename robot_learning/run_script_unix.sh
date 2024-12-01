d62073139a065a07f67e520a800bf524069f61f2
Muhammad540 <thisisdeahmed@gmail.com>
2024-12-01 17:08:46 +0000

    delete robot_learning folder remapped

--- robot_learning/run_script_unix.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-PROJECT_NAME=$(basename "$SCRIPT_DIR")
-
-reset
-make clean
-make
-
-./"$PROJECT_NAME"
--- robot_learning/visualize_model.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-PROJECT_NAME=$(basename "$SCRIPT_DIR")
-
-cd /opt/mujoco/mujoco-3.2.5/bin/
-
-# below change the xml file to the one you want to visualize 
-./simulate "../../../../home/ros2-dev/mnt/ws/$PROJECT_NAME/model/ur5e.xml"
